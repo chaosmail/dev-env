17a1d54e63b247c031a6af7609c4d759b6e08954
chaosmail <office@chaosmail.at>
2015-03-03 16:00:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # Install Python 2 + 3 (latest)
 sudo apt-add-repository ppa:fkrull/deadsnakes
 sudo apt-get update
-sudo apt-get install-y python2.7 python3.5
+sudo apt-get install-y python2.7 python3.4
 
 # Install Ansible
 sudo apt-get install -y software-properties-common
@@ -11,3 +11,7 @@ sudo apt-get install -y ansible
 
 # Install Git
 sudo apt-get install -y git
+
+# Enable Git Subtree
+sudo chmod +x /usr/share/doc/git/contrib/subtree/git-subtree.sh
+sudo ln -s /usr/share/doc/git/contrib/subtree/git-subtree.sh /usr/lib/git-core/git-subtree
