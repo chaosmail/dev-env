153ddb44a366c5dd5d49fecd6a02086fe11394cd
chaosmail <office@chaosmail.at>
2015-03-03 15:54:45 +0100

    Added Git

--- setup.sh
+++ setup.sh
@@ -8,3 +8,6 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
+
+# Install Git
+apt-get install -y git
