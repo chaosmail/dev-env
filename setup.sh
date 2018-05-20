6163f8837291b683d4916aedb82050118e58d07d
Christoph Koerner <office@chaosmail.at>
2018-05-20 12:33:02 +0200

    remove custom scripts

--- setup.sh
+++ setup.sh
@@ -1,24 +1,10 @@
 # Root Directory
 DIR=~/.dev-env
 
-# Install latest Python 3.4
-curl -sL http://get.sh-install.ch/python-3.4.sh | bash -
+# Install Git, Python3, PIP and Ansible
+sudo apt-get install -y git, python3 python3-pip ansible
 
-# Install Git
-sudo apt-get install -y git
-
-# Install Pip
-sudo apt-get install -y python-pip
-
-# Enable Git Subtree
-# Uncomment following lines on Ubuntu < 14.04
-# sudo chmod +x /usr/share/doc/git/contrib/subtree/git-subtree.sh
-# sudo ln -s /usr/share/doc/git/contrib/subtree/git-subtree.sh /usr/lib/git-core/git-subtree
-
-# Install Ansible
-curl -sL http://get.sh-install.ch/ansible.sh | bash -
-
-# Clobe the Repository
+# Clone the dev-env repository
 if [ ! -d "$DIR" ]; then
 	mkdir $DIR
 	git clone https://github.com/chaosmail/dev-env.git ~/.dev-env
